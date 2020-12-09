b0e09709d31deb8dd0d49799baee641ca651976c
Wildstrup <60379913+Wildstrup@users.noreply.github.com>
2020-12-09 01:17:28 +0100

    Update

--- header.php
+++ header.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link href="The texttype we want" rel="stylesheet">
+    <link href="The text type we want" rel="stylesheet">
     <script src="functions.js" defer> </script>
     <meta charset="utf-8">
     <title>OB's tidsline</title>
   </head>
   <body>
     <header>
+      <div class="box">
+        <div class="wrapper">
+          <nav>
 
+          </nav>
+        </div>
+      </div>
     </header>
