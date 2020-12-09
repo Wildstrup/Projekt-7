b77b2b90259bfdd962accdd84d22237d94488cff
Wildstrup <60379913+Wildstrup@users.noreply.github.com>
2020-12-10 00:14:31 +0100

    Update

--- header.php
+++ header.php
@@ -11,11 +11,43 @@
   </head>
   <body>
     <header>
-      <div class="box">
-        <div class="wrapper">
-          <nav>
-
-          </nav>
+      <div class="banner">
+        <div class="firstBit">
+          Sammen for Klubben.
+        </div>
+        <div class="secondBit">
+          For byen. For altid.
         </div>
       </div>
     </header>
+    <nav>
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+      <ul class="navLinks">
+        <li>
+          <a href="#">Kampe</a>
+        </li>
+        <li>
+          <a href="#">Førsteholdet</a>
+        </li>
+        <li>
+          <a href="#">Fans</a>
+        </li>
+        <li>
+          <a href="#">Akademi
+        </li>
+        <li>
+          <a href="#">Øens hold</a>
+        </li>
+        <li>
+          <a href="#">Business</a>
+        </li>
+        <li>
+          <a href="#">Fanshop</a>
+        </li>
+        <li>
+          <a href="#">Mere</a>
+        </li>
+      </ul>
+    </nav>
