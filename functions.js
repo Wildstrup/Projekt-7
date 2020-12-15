585f405b61e7cb60f3a7a75c1d980b7877b191e0
Wildstrup <60379913+Wildstrup@users.noreply.github.com>
2020-12-15 11:03:19 +0100

    Update

--- functions.js
+++ functions.js
@@ -7,7 +7,48 @@ let navSlide = () => {
     nav.classList.toggle('navActive');
     burgerMenu.classList.toggle('toggle');
   });
-
 }
 
 navSlide();
+
+
+let submit = document.querySelector('.noteSubmit');
+let noteList = document.querySelector('.notes');
+let noteInput = document.querySelector('.noteInput');
+
+function template(data) {
+  noteList.insertAdjacentHTML("beforeend",`
+  <div class="notes">
+    <div class="note flex itemsStart justifyStart">
+      <div class="flex1">
+        <h3 class="yourNote">${data.yourNote}</h3>
+        <div class="noteBody">${data.note}</div>
+      </div>
+    </div>
+  `);
+}
+
+function appendNote(event) {
+  let data = {
+    note: noteInput.value,
+    yourNote: "Note"
+  }
+  event.preventDefault();
+
+  if (noteInput.value.length < 1) return;
+
+  template(data);
+
+  noteInput.value = "";
+
+  localStorage.setItem('noteListing', noteList.innerHTML);
+}
+
+submit.addEventListener('click', appendNote, false);
+
+
+let saved = localStorage.getItem('noteListing');
+
+if (saved) {
+  noteList.innerHTML = saved;
+}
