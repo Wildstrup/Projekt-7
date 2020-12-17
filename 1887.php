1525e230d2de9ef276d99db2bb11071c4443aa23
Wildstrup <60379913+Wildstrup@users.noreply.github.com>
2020-12-17 02:12:05 +0100

    Update

--- 1887.php
+++ 1887.php
@@ -21,6 +21,43 @@
     </div>
   </section>
 
+  <article>
+    <h2>Opstarten af klubben</h2>
+    <img class="widePicture" src="img/oldtimes.jpg" alt="Et gammelt billede fra OB">
+    <div class="textBox">
+      <p class="mainText">OB blev stiftet i 1887 og  startede ud
+        med at være en Cricket klub. Det er
+        begyndelsen på OB's historie.</p>
+    </div>
+    <div class="textBox2">
+      <img class="tallPicture" src="img/thefounder.jpg" alt="Et billede af Max Schmidt">
+      <p class="mainText">Manden der var med til at starte det hele, var
+        OB's første formand Max Schmidt</p>
+    </div>
+  </article>
+
+  <article>
+    <h2>Fodbolden i OB</h2>
+    <img class="widePicture" src="img/oldtimes.jpg" alt="Et gammelt billede fra OB">
+    <div class="textBox">
+      <p class="mainText">OB var en af de første klubber, som
+        implementerede fodbold ind i
+        deres klub. Det var i 1889 Odenses
+        fodboldeventyr startede.</p>
+    </div>
+  </article>
+
+  <article>
+    <h2>Større tider</h2>
+    <img class="widePicture" src="img/munkemose.jpg" alt="Miraklet i Madrid">
+    <div class="textBox">
+      <div class="textBox">
+        <p class="mainText">I 1894 blev der bygget videre på
+          klubben og fik derfor også tildelt
+          deres eget område i Munke Mose.</p>
+    </div>
+  </article>
+
   <section class="noteSection">
     <div class="noteContainer">
       <h3 class="noteContainerTitle">Dine notater</h3>
@@ -44,7 +81,6 @@
       </div>
     </div>
   </section>
-</main>
 
 <?php
   include 'footer.php';
